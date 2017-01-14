dc1641036d40e3cfd98b8168324f8c350dac9417
王顶 <408542507@qq.com>
2017-01-14 22:33:10 +0800

    修改了代码

--- step09.js
+++ step09.js
@@ -2,7 +2,7 @@
  * Created by 王顶 on 2017/1/10.
  */
 var http = require('http');
-var bl = require('bl');
+
 var url1 = process.argv[2];
 var url2 = process.argv[3];
 var url3 = process.argv[4];
@@ -10,41 +10,38 @@ var datas = ['0', '0', '0'];
 var num = 0;
 
 function pro(n, data) {
-    if(isFull()) {
-        datas.forEach(function (data) {
-            console.log(data);
-        });
-        return;
-    }
-    datas[n] = data;
+    if(num == 3)    return datas.forEach(console.log);
 }
 
-function isFull() {
-    var is = true;
-    datas.forEach(function (data) {
-        if(data == '0') return false;
-    })
-}
 http.get(url1, function (res) {
     res.setEncoding('utf-8');
-    res.pipe(bl(function (err, data) {
-        if(err) return console.log(err);
-        pro(0, data.toString());
-    }));
+    res.on('data', function (data) {
+        datas[0] += data;
+    });
+
+    res.on('end', function () {
+       pro(++num, datas);
+    });
 });
 
 http.get(url2, function (res) {
     res.setEncoding('utf-8');
-    res.pipe(bl(function (err, data) {
-        if(err) return console.log(err);
-        pro(1, data.toString());
-    }));
+    res.on('data', function (data) {
+        datas[1] += data;
+    });
+
+    res.on('end', function () {
+       pro(++num, datas);
+    });
 });
 
 http.get(url3, function (res) {
     res.setEncoding('utf-8');
-    res.pipe(bl(function (err, data) {
-        if(err) return console.log(err);
-        pro(2, data.toString());
-    }));
+    res.on('data', function (data) {
+        datas[2] += data;
+    });
+
+    res.on('end', function () {
+       pro(++num, datas);
+    });
 });

d55f9537fca71f0cbc083d3c62cdff55084f0af1
王顶 <408542507@qq.com>
2017-01-19 07:03:46 +0800

    增加了 mocha 的 democode

---
+++ DemoCode/mocha-demo/add.js
@@ -0,0 +1,9 @@
+/**
+ * Created by 王顶 on 2017/1/18.
+ */
+
+function add(x, y) {
+    return x + y;
+}
+
+module.exports = add;
---
+++ DemoCode/mocha-demo/add.test.js
@@ -0,0 +1,12 @@
+var add = require('./add');
+var expect = require('chai').expect;
+
+describe('加法函数的测试', function () {
+    it('1 加 1 应该等于 2', function () {
+        expect(add(1, 1)).to.be.equal(2);
+    });
+
+    it('3 加 -3 应该等于 0', function () {
+        expect(add(3, -3)).to.be.equal(0);
+    });
+});

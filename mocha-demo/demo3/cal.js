2d3850c2bcb45a2b0e186cafc2943d2003631f9b
wangding <408542507@qq.com>
2017-04-20 21:54:07 +0800

    feat: add demo3

---
+++ mocha-demo/demo3/cal.js
@@ -0,0 +1,22 @@
+/**
+ *  add two number x and y
+ *  @param {number} x The first number to add
+ *  @param {mumber} y The second number to add
+ *  @return {number} The result x + y
+ */
+function add(x, y) {
+  return x + y;
+}
+
+/**
+ *  multiply two number x and y
+ *  @param {number} x The first number to multiply
+ *  @param {mumber} y The second number to multiply
+ *  @return {number} The result x * y
+ */
+function multiply(x, y) {
+  return x * y;
+}
+
+module.exports.add = add;
+module.exports.multiply = multiply;
---
+++ mocha-demo/demo3/test/dir/multiply.test.js
@@ -0,0 +1,16 @@
+var multiply = require('../../cal').multiply;
+var expect = require('chai').expect;
+
+describe('unit test of multiply', function() {
+  it('0*0 = 0', function() {
+    expect(multiply(0, 0)).to.be.equal(0);
+  });
+
+  it('1*1 = 1', function() {
+    expect(multiply(1, 1)).to.be.equal(1);
+  });
+
+  it('1*0 = 0', function() {
+    expect(multiply(1, 0)).to.be.equal(0);
+  });
+});

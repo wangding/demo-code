7058720ca9a919671569ca379e3174171147d7d6
wangding <408542507@qq.com>
2017-09-19 17:32:36 +0800

    add demo code

--- node/13-http/01-server.js
+++ node/13-http/01-server.js
@@ -4,9 +4,9 @@ var server = require('http').createServer();
 server.on('request', function (req, res) {
   console.log('REQUEST: ' + JSON.stringify(req.headers));
   req.pipe(process.stdout);
-  res.end();
+  res.end('hello');
 });
 
 server.listen(8080, function () {
-  console.log('Listening on %d.', this.address().port)
+  console.log('Listening on:', this.address().port);
 });
--- node/14-dns/01-my-nslookup.js
+++ node/14-dns/01-my-nslookup.js
@@ -2,7 +2,12 @@
 var dns = require('dns');
 
 var ns = process.argv[2];
-
+/*
 dns.lookup(ns, function(err, address) {
   console.log(address);
+});
+*/
+
+dns.resolve(ns, function(err, addr) {
+  console.log(addr);
 });

e7e6f3f8468e1e7c6ab3b5468cc78a75ea47ba9c
wangding <408542507@qq.com>
2017-10-19 23:06:45 +0800

    add demo code

---
+++ node/13-http/03-get-repos.js
@@ -0,0 +1,31 @@
+#!/usr/bin/env node
+
+var https = require('https'),
+    url  = require('url');
+
+var addr = 'https://api.github.com/search/repositories?q=user:' + (process.argv[2] || 'wangding');
+
+var options = url.parse(addr);
+
+options.method = 'GET';
+options.headers = {
+    'User-Agent': '03-get-repos.js'
+};
+
+https.get(options, function(res) {
+  var result = '';
+
+  res.on('data', function(data) {
+    result += data.toString('utf8');
+  });
+  
+  res.on('end', function() {
+    var reps = JSON.parse(result);
+
+    console.log('Total:', reps.total_count);
+    console.log('==========================');
+    for(var i=0; i<reps.total_count; i++) {
+      console.log('%d\t%s', i, reps.items[i].name);
+    }
+  });
+});

5d93eb20132e9832a5f10cafff0c84b997a74288
wangding <408542507@qq.com>
2017-10-24 11:01:36 +0800

    edit 01-exec-file.js

--- node/15-child-process/01-exec-file.js
+++ node/15-child-process/01-exec-file.js
@@ -1,13 +1,21 @@
 #!/usr/bin/node
 
-var cp = require('child_process');
+var cp = require('child_process'),
+    cmd = '',
+    arg = [];
+
+if(typeof process.argv[2] === 'undefined') {
+  cmd = 'cat';
+  arg.push(__filename);
+} else {
+  cmd = process.argv[2];
+  arg = process.argv.slice(3, process.argv.length);
+}
 
 console.log('I am father process. PID:', process.pid);
-console.log('cat 01-exec-file.js\n');
 
-cp.execFile('cat', ['01-exec-file.js'], function(err, stdout, stderr) {
+cp.execFile(cmd, arg, function(err, stdout, stderr) {
   if(err) console.error(err);
   
   console.log(stdout);
 });
-

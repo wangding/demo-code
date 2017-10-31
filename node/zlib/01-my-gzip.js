4a2e41296e9f42f12330630e2a2ee0a8b9d2cd33
wangding <408542507@qq.com>
2017-10-31 19:07:30 +0800

    add demo code

--- node/zlib/01-my-gzip.js
+++ node/zlib/01-my-gzip.js
@@ -12,9 +12,26 @@ var dst = src + '.gz';
 //fs.createReadStream(src).pipe(zlib.createGzip()).pipe(fs.createWriteStream(dst));
 
 var buf = fs.readFileSync(src);
-
+/*
 zlib.gzip(buf, function(err, data) {
   console.log('%s size: %d byte', src, buf.length);
   console.log('%s size: %d byte', dst, data.length);
   fs.writeFileSync(dst, data);
 });
+
+var data = zlib.gzipSync(buf);
+var data = zlib.deflateSync(buf);
+
+console.log('%s size: %d byte', src, buf.length);
+console.log('%s size: %d byte', dst, data.length);
+fs.writeFileSync(dst, data);
+
+zlib.deflate(buf, function(err, data) {
+  console.log('%s size: %d byte', src, buf.length);
+  console.log('%s size: %d byte', dst, data.length);
+  fs.writeFileSync(dst, data);
+});
+*/
+
+fs.createReadStream(src).pipe(zlib.createDeflate()).pipe(fs.createWriteStream(dst));
+
--- node/zlib/02-my-gunzip.js
+++ node/zlib/02-my-gunzip.js
@@ -13,10 +13,25 @@ var dst = src.slice(0, src.length -3);
 //fs.createReadStream(src).pipe(zlib.createUnzip()).pipe(fs.createWriteStream(dst));
 
 var buf = fs.readFileSync(src);
-
+/*
 zlib.gunzip(buf, function(err, data) {
   console.log('%s size: %d byte', src, buf.length);
   console.log('%s size: %d byte', dst, data.length);
   fs.writeFileSync(dst, data);
 });
 
+var data = zlib.gunzipSync(buf);
+var data = zlib.inflateSync(buf);
+
+console.log('%s size: %d byte', src, buf.length);
+console.log('%s size: %d byte', dst, data.length);
+fs.writeFileSync(dst, data);
+
+zlib.inflate(buf, function(err, data) {
+  console.log('%s size: %d byte', src, buf.length);
+  console.log('%s size: %d byte', dst, data.length);
+  fs.writeFileSync(dst, data);
+});
+*/
+
+fs.createReadStream(src).pipe(zlib.createInflate()).pipe(fs.createWriteStream(dst));

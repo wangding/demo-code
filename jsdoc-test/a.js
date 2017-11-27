8fadcaef6fe9d2df6ff6f042fea56b315fa9c729
wangding <408542507@qq.com>
2017-11-27 16:47:01 +0800

    add jsdoc demo code

---
+++ jsdoc-test/a.js
@@ -0,0 +1,19 @@
+/**
+ * foo
+ *
+ * @returns {undefined}
+ */
+function foo() {
+  
+}
+
+/**
+ * 实现加法运算
+ *
+ * @param {integer} 加数 x
+ * @param {integer} 加数 y
+ * @returns {integer} 求和结果
+ */
+function add(x, y) {
+
+}

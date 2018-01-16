8ec761d4969b1ba8d623451e8c88aa18aec7af17
wangding <408542507@qq.com>
2018-01-16 17:11:54 +0800

    add demo code

---
+++ node/test/javascript-basic-type.js
@@ -0,0 +1,8 @@
+console.log(typeof 3.14);
+console.log(typeof true);
+console.log(typeof '3.14');
+console.log(typeof { 'key': 3.14 });
+console.log(typeof Object);
+console.log(typeof undefine);
+console.log(typeof null);
+

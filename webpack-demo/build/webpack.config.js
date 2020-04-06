26aa91c47a5b87e1e57a48d834335871d02812ff
wangding <408542507@qq.com>
2020-04-06 10:49:33 +0800

    webpak demo

---
+++ webpack-demo/build/webpack.config.js
@@ -0,0 +1,16 @@
+// webpack.config.js
+
+const path = require('path');
+module.exports = {
+  mode: 'development', // 开发模式
+  entry: path.resolve(__dirname, '../src/main.js'),    // 入口文件
+  output: {
+    filename: '[name].[hash:8].js',      // 打包后的文件名称
+    path: path.resolve(__dirname, '../dist')  // 打包后的目录
+  },
+  plugins:[
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, '../public/index.html')
+    })
+  ]
+}

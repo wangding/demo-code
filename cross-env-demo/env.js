173a77ee8e8b96956d146fa6d67f99c50d5a9b92
wangding <408542507@qq.com>
2020-07-11 16:47:41 +0800

    add cross-env demo

---
+++ cross-env-demo/env.js
@@ -0,0 +1,16 @@
+// run 1:
+// export NODE_ENV=abc
+// echo $NODE_ENV
+
+// run 2:
+// npm run dev
+
+// run 3:
+// npm run test
+
+// run 4:
+// npm run prod
+
+// run 5:
+// npm run parentScript
+console.log(process.env.NODE_ENV)

63231ab8869b267720958d871a326374b5bccde2
wangding <408542507@qq.com>
2022-02-06 05:43:20 +0800

    feat: thread

---
+++ thread/worker2.js
@@ -0,0 +1,11 @@
+function fb(n) {
+  if(n===1 || n===2) {
+    return 1;
+  } 
+  return fb(n-1) + fb(n-2);
+}
+
+console.time('fb执行时间');
+let result = fb(43);
+console.timeEnd('fb执行时间');
+self.postMessage('worker2');

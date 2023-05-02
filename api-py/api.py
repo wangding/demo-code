ad2edc6f5956f8adeca7ae6ee90ee34dd4e48272
wangding <408542507@qq.com>
2023-05-03 07:35:30 +0800

    python http api with flask

---
+++ api-py/api.py
@@ -0,0 +1,48 @@
+from flask import Flask, render_template, jsonify, request
+from urllib.parse import unquote
+
+from decouple import config
+
+import os
+import nltk
+nltk.data.path.append('nltk_data')
+
+#os.environ["OPENAI_API_KEY"] = config('OPENAI_API_KEY')
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/api/data', methods=['GET','POST'])
+def get_data():
+    if request.method == 'GET':
+        sample_data = {
+            'message': 'Hello, Flask API!',
+            'data': [1, 2, 3, 4, 5]
+        }
+        print ("DEBUG",sample_data)
+        return jsonify(sample_data)
+    elif request.method == 'POST':
+        print ("DEBUG request",request)
+        encode_url = unquote(unquote(request.args.get('url')))
+        print ("DEBUG encode_url",encode_url)
+        if not encode_url:
+            return jsonify({'error': 'URL is required'}), 400
+
+        decoded_text = decode_website(encode_url)
+
+        print ("DEBUG decoded_text",decoded_text)
+
+        summary = summarize_webpage(decoded_text)
+
+        response = {
+            'submitted_url': encode_url,
+            'summary': summary,
+        }
+
+        return jsonify(response)
+
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 5001))
+    app.run(debug=True, host='0.0.0.0', port=port)
